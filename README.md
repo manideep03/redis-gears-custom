ad8bf45012d7987b2e8717e5357b698aac2f3bb4
Manideep Gujjari <manideep.gujjari@gmail.com>
2023-12-30 22:49:08 +0530

    instalation test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 * Redis Gears pipelines for write behind implementation
+
+Custom instalation 
+
+`pip install git+https://github.com/manideep03/redis-gears-custom.git`
--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '0.0.1'
 DESCRIPTION = 'Base package to test custom rgsync'
 LONG_DESCRIPTION = 'Base version'
