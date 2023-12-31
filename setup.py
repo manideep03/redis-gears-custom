480c41332fc818e1cf2ec12ff2230ea3ad418f48
Manideep Gujjari <manideep.gujjari@gmail.com>
2023-12-31 10:26:48 +0530

    utils wrapping

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Base package to test custom rgsync'
 LONG_DESCRIPTION = 'Base version'
 
