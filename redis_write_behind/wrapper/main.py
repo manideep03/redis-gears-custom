d844d6fba05b9c5bec1ab2c4d2de34871c6dc408
Manideep Gujjari <manideep.gujjari@gmail.com>
2024-01-01 11:46:22 +0530

    reformat to align

--- redis_write_behind/wrapper/main.py
+++ redis_write_behind/wrapper/main.py
@@ -2,8 +2,15 @@ from redis_write_behind.utils.db_connectors import MySqlConnection, MySqlConnect
 from redis_write_behind.pipelines import KeyValueWriteBehind, ListWriteBehind, HashWriteBehind
 
 class WBJobBuilder():
-    def __init__(self, GB, jobConfig:dict) -> str:
+    def __init__(self, GB, jobConfig:dict):
+        self.__config = jobConfig
+        self.__GB = GB
         self.__validateJob(jobConfig)
+    
+    def create(self) -> str:
+        jobConfig = self.__config
+        GB = self.__GB
+        
         dt = jobConfig['targetDataType'].lower()
         keyFlag = False
         if 'primaryCacheKey' in jobConfig.keys() and jobConfig['primaryCacheKey'] == 1:
@@ -13,21 +20,24 @@ class WBJobBuilder():
         
         if dt == 'hash':
             HashWriteBehind(GB, connector=__conn, keysPrefix=jobConfig['keysPrefix'], name=jobConfig['name'], version=jobConfig['version'], mappings=jobConfig['keysMapping'], primaryCacheKey=keyFlag)
+            return "OK"
         if dt == 'list':
             ListWriteBehind(GB, connector=__conn, keysPrefix=jobConfig['keysPrefix'], name=jobConfig['name'], version=jobConfig['version'], mappings=jobConfig['keysMapping'])
+            return "OK"
         if dt == 'keyValue':
             KeyValueWriteBehind(GB, connector=__conn, keysPrefix=jobConfig['keysPrefix'], name=jobConfig['name'], version=jobConfig['version'], mappings=jobConfig['keysMapping'])
+            return "OK"
         else:
-            raise Exception("%s target datatype not compatible".format(dt))
+            raise Exception("{} target datatype not compatible".format(dt))
 
     def __validateJob(self, config:dict):
         __mandatoryCol = ['name', 'version', 'keysPrefix', 'targetDataType', 'keysMapping', 'dbConfig']
         __mandatoryColForDB = ['host', 'user', 'password', 'tableName', 'primaryKey']
         for i in __mandatoryCol:
-            if i not in config.keys(): raise Exception('%s not found in config'.format(i))
+            if i not in config.keys(): raise Exception('{} not found in config'.format(i))
 
         for i in __mandatoryColForDB:
-            if i not in config['dbConfig'].keys(): raise Exception('%s not found in dbConfig'.format(i))
+            if i not in config['dbConfig'].keys(): raise Exception('{} not found in dbConfig'.format(i))
 
     def __prepareConnector(self, config:dict) -> MySqlConnector:
         return MySqlConnector(MySqlConnection(config['dbConfig']['user'],
