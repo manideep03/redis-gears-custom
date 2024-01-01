6369a294402a0400ec2e4768827a3328138f4cf2
Manideep Gujjari <manideep.gujjari@gmail.com>
2024-01-01 11:39:26 +0530

    vresion

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-VERSION = '0.4.3'
+VERSION = '0.4.4'
 DESCRIPTION = 'Base package to test custom redis write behind gears pipelines'
 LONG_DESCRIPTION = 'Version 0.4'
 
